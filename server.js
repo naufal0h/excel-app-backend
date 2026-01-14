4ce4718ee055fcd1bda73253b0769cf74eb940ac
Naufal <naufal.hertantyo@gmail.com>
2026-01-14 11:33:33 +0700

    excel-app-backend

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const xlsx = require('xlsx');
+const fs = require('fs');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const upload = multer({ dest: 'uploads/' });
+
+// Database sederhana menggunakan file JSON
+const DB_FILE = 'database.json';
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    const workbook = xlsx.readFile(req.file.path);
+    const data = {};
+
+    workbook.SheetNames.forEach(sheetName => {
+        data[sheetName] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    });
+
+    // Simpan ke "Database"
+    let currentDB = [];
+    if (fs.existsSync(DB_FILE)) {
+        currentDB = JSON.parse(fs.readFileSync(DB_FILE));
+    }
+    currentDB.push({ id: Date.now(), filename: req.file.originalname, content: data });
+    fs.writeFileSync(DB_FILE, JSON.stringify(currentDB));
+
+    res.json({ message: "Data berhasil disimpan!", data });
+});
+
+app.get('/data', (req, res) => {
+    if (fs.existsSync(DB_FILE)) {
+        res.json(JSON.parse(fs.readFileSync(DB_FILE)));
+    } else {
+        res.json([]);
+    }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
